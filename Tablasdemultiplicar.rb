7e875e20a23775275576b7e11ec5e713db39e8ab
Luis Escobar Salas <luis.escobar.s@usach.cl>
2023-06-19 21:03:01 -0400

    Segundo Commit

---
+++ Tablasdemultiplicar.rb
@@ -0,0 +1,12 @@
+
+for numero  in (1..10) do
+    
+    puts("Tabla del #{numero} *")
+
+    11.times do |i|
+    string = "#{numero} * #{i}"
+    puts "#{string} = #{numero * i}"
+    ##print(numero)   
+end
+
+end
