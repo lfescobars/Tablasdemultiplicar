83dd895dccc2e22aa471aee08e5ecc777976151f
Luis Escobar Salas <luis.escobar.s@usach.cl>
2023-06-19 21:14:20 -0400

    Cuarto commit: agregar archivo times_anidado.rb

--- Tablasdemultiplicar.rb
+++ Tablasdemultiplicar.rb
@@ -1,12 +1,7 @@
-
 for numero  in (1..10) do
-    
     puts("Tabla del #{numero} *")
-
     11.times do |i|
     string = "#{numero} * #{i}"
-    puts "#{string} = #{numero * i}"
-   
+    puts "#{string} = #{numero * i}"  
 end
-
 end
---
+++ times_anidado.rb
@@ -0,0 +1,7 @@
+n = 3
+n.times do |i|
+ puts"**********"
+ 11.times do |j|
+ puts "#{i} * #{j} = #{i * j}"
+ end
+end
